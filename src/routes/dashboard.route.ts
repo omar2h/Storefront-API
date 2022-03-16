ef6b3ed34fb3d4d9f8ca8db0f256be934a93a915
omarsamak <omarhesham83@gmail.com>
2022-03-16 14:22:45 +0200

    refactor: change dashboard routes

--- src/routes/dashboard.route.ts
+++ src/routes/dashboard.route.ts
@@ -8,9 +8,9 @@ import {
 } from '../controllers/dashboard.controller'
 import authenticationMiddleware from '../middleware/auth.middleware'
 
-router.route('/get-orders-products').get(authenticationMiddleware, getAllOrders)
+router.route('/orders-products').get(authenticationMiddleware, getAllOrders)
 
-router.route('/get-orders-products/:id').get(authenticationMiddleware, getOrder)
+router.route('/orders-products/:id').get(authenticationMiddleware, getOrder)
 router.route('/user-orders/:id').get(authenticationMiddleware, getUserOrders)
 
 export default router
